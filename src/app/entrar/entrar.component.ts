10dd4d183530332589c826ac4784aa1e5ce000b4
vnalmenara <veronica_n_almenara@hotmail.com>
2021-03-05 14:23:15 -0300

    implementacao-tipo-adm-user

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -39,6 +39,7 @@ export class EntrarComponent implements OnInit {
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
+      environment.tipo = this.userLogin.tipo
       
       this.router.navigate(['/inicio'])}, erro => {
       if(erro.status == 500){
